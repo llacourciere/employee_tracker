a8d0b9e8c0bd2670a1ea18da5e9c19ad502cfa89
LaLacou <108777796+users.noreply.github.com>
2022-09-30 11:28:34 -0700

    created database and department table

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO departments (name)
+VALUES
+    ('IT'),
+    ('Marketing'),
+    ('Finance'),
+    ('HR'),
+    ('Service'),
+    ('Facilities'),
+    ('Sales'),
+    ('Design'),
+    ('Legal');
